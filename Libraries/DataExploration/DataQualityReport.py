0cb710a49dc757d1943affe74700a74951ab48b8
SadWheatFarmer <smutnyjw@gmail.com>
2022-03-06 09:55:36 -0500

    PROJECT1: DataQualityReport
    1) Improved the DataQualityReport.py to account for input of column of strings.
    2) Added ability to publish DataQualityReport for the final day summaries.
    3) Made other documentation improvements.
    4) .pyc file: Corrected the location that code imports the DQR.py file.

--- Libraries/DataExploration/DataQualityReport.py
+++ Libraries/DataExploration/DataQualityReport.py
@@ -1,12 +1,30 @@
 ############################################################################
-#   Contributor: John Smutny
-#   Original Author: Dr Creed Jones
-#   Date Updated: 03/05/2022
+#   Author: John Smutny
+#   Contributor: Dr Creed Jones
+#   Date Updated: 03/06/2022
 #   Date Created: 02/15/2022
 #
-#   statsdf     data frame summarizing the Data Quality Report from a dataset.
+#   Description:
+#       Class dedicated to producing an informative Data Quality Report to
+#       the console. This report should be used to better understand a dataset.
+#
+#       User must use the pandas.DataFrame object to insert columns of
+#       features for processing.
+#
+#   Future Improvements:
+#       1) Add ability to process columns of 'char' data values. Test Case: if
+#       user attempts to '.addCol()' of 'chars' (A, B, C, etc); 1) the code
+#       could convert those chars to ints using the ascii table, 2) do data
+#       process as if it was a numeric for all non-float metrics like 'mean',
+#       and 3) convert the resulting ints back to chars using the ascii table.
+#
+#       2) Add ability to publish DataQualityReport as a csv.
+#
+#   Parameters:
+#       statsdf data frame summarizing the Data Quality Report from a dataset.
 ############################################################################
 import pandas
+
 
 class DataQualityReport:
     # Contructor. Define what values are being calculated for the stats report.
@@ -25,27 +43,28 @@ class DataQualityReport:
                                 'n_missing']
         pass
 
-    # Add a 'feature' to be included on the report. Append columns by 1.
+    ##################################################################
+    # @input label    str column header to be added to the DQR.
+    # @input data     DataFrame column of data to be processed. SINGLE COLUMN.
+    # Des: Add a 'feature' to be included on the report. Append columns by 1.
     def addCol(self, label, data):
         cardinalityV = len(data.unique())
         n_missing = data.isnull().sum()
 
-        #TODO - Divide this 'addCol' fct into the numerics, strs and others.
-        #strEx = "some str"
-       # print(type(data[0]))
-        #strDataType = type(data[0])
-        #strEx.index('str') == 0
-        if 0:
+        # Only process columns of data that are 1) Numeric & 2) All have values.
+        if type(data[0]) == str:
             # Data is a string type. Do not perform mathematical operations.
-            print("ERROR: DataQualityReport.addCol: Entered data is not a"
+            print("WARN: DataQualityReport.addCol: Entered data is not a "
                   "numeric type. Setting relevant dataQuality values to zero")
-            meanV = 0
-            medianV = 0
-            modeV = 0
-            stdDev = 0
-            minV = 0
-            maxV = 0
-
+            REPLACEMENT_VALUE = "NA"
+            meanV = REPLACEMENT_VALUE
+            medianV = REPLACEMENT_VALUE
+            modeV = REPLACEMENT_VALUE
+            stdDev = REPLACEMENT_VALUE
+            minV = REPLACEMENT_VALUE
+            maxV = REPLACEMENT_VALUE
+            n_medianV = REPLACEMENT_VALUE
+            n_modeV = REPLACEMENT_VALUE
         else:
             meanV = data.mean()
             medianV = data.median()
@@ -86,5 +105,8 @@ class DataQualityReport:
                                n_zeros,
                                n_missing]
 
+    ##################################################################
+    # @output str    String of the dataFrame.
+    # Des: Publish DataQualityReport to the console
     def to_string(self):
         return self.statsdf.to_string()
--- Project1-NOAA_Weather_Model/pr1_DataPreperation.py
+++ Project1-NOAA_Weather_Model/pr1_DataPreperation.py
@@ -14,32 +14,30 @@
 #       Read in a csv, create an initial dataframe of data with headers.
 #       Identify every unique Day and process some ELEMENT possibilities into
 #           a single list. Then place that list into a resulting dataframe.
+#       Publish a DataQualityReport for a day's summary of data.
 #
 #   Cannot:
 #       'DataQualityReport.py' lib file cannot handle string entries.
-#       Publish a DataQualityReport for initial or data summary dataframe.
+#       Publish a DataQualityReport for initial data.
 #       End Goal headers do not contain units.
 #       Process all possible ELEMENT values for an unique Day's events
 ##########################################################################
 
 import pandas
 import numpy as np
-from Libraries.DataExploration.DataQualityReport import DataQualityReport
+from vt_ApplicationsOfML.Libraries.DataExploration.DataQualityReport import \
+    DataQualityReport
 
 #####################################
 # Initial loading of header-less data and adding a header
 #   Date = yyyymmdd
 #   OBS-Time = hhmm
 
-header = ['ID', 'Date', 'Element', 'Value', 'MeasurementFlag',
-          'QualityFlag', 'SourceFlag', 'OBS-Time']
-# filename = 'C:/Data/USW00013880-Test.xlsx'
-# df = pandas.read_excel(filename, sheet_name='TestData')
-# df.columns = header
-
+INIT_HEADER = ['ID', 'Date', 'Element', 'Value', 'MeasurementFlag',
+               'QualityFlag', 'SourceFlag', 'OBS-Time']
 filename = 'C:/Data/USW00013880-Test.csv'
 df = pandas.read_csv(filename, header=None)
-df.columns = header
+df.columns = INIT_HEADER
 
 print(df[1:5])
 
@@ -48,18 +46,20 @@ print(df[1:5])
 # Create an organized data set summary for the console using a data frame.
 report1 = DataQualityReport()
 
-for thisLabel in header:  # for each column, report basic stats
-    if thisLabel == 'Date':
-        print('only do Date col for now')
+for thisLabel in INIT_HEADER:  # for each column, report basic stats
+    if thisLabel == 'MeasurementFlag' \
+            or thisLabel == 'QualityFlag' \
+            or thisLabel == 'SourceFlag':
+        print('WARN: DataQualityReport.py cannot process label = ' + thisLabel)
+        # TODO - 'DataQualityReport.py' cannot process 'nan' values.
+        #  Clean data before this step? (fill in nan, replace categorical chars)
+        continue
+    else:
         thisCol = df[thisLabel]
         report1.addCol(thisLabel, thisCol)
-    else:
-        # TODO - Figure how to stop Str from being read in DataQualityReport
-        continue
 
-
+print("DataQualityReport - 1/2")
 print(report1.to_string())
-
 
 ##########################################################################
 # TODO - Add logic to remove data points from the starting dataset.
@@ -80,9 +80,9 @@ elementTypes = df.Element.unique()
 targetVariables = ['PRECIPFLAG', 'PRECIPAMT', 'NEXTDAYPRECIPFLAG',
                    'NEXTDAYPRECIPAMT']
 
-newHeader = baseColumns + elementTypes.tolist() + targetVariables
-#print("New Headers:")
-#print(newHeader)
+DETAILED_HEADER = baseColumns + elementTypes.tolist() + targetVariables
+# print("New Headers:")
+# print(DETAILED_HEADER)
 
 # 2) Isolate a single day's total events.
 daysMeasured = df.Date.unique()
@@ -94,11 +94,11 @@ print(oneDayEvents)
 
 # 3) Summarize the day's events into a single entry
 #       Create a blank entry.
-keys = newHeader
-blankValues = [0] * len(newHeader)
+keys = DETAILED_HEADER
+blankValues = [0] * len(DETAILED_HEADER)
 entry = dict(zip(keys, blankValues))
-#print("Blank Entry pre-processing")
-#print(entry)
+# print("Blank Entry pre-processing")
+# print(entry)
 
 # 4) Loop through each datapoint on a particular day. Find what part of the
 #       dictionary the datapoint's ELEMENT corresponds too. Do math based on
@@ -108,22 +108,22 @@ for i in range(numEntries):
     entryList = oneDayEvents.loc[i, :].values.tolist()
     # TODO - ^^^ Maybe make 'entryList' a map of the keys equal to initial
     #  'header' to simply value accessing and placement?
-    #print(i)
-    #print(entryList)
+    # print(i)
+    # print(entryList)
     entry['Date'] = entryList[1]
     element = entryList[2]
 
     # SWITCH statement to process the ELEMENT + VALUE
     if element == 'TMAX':
         entry[element] = entryList[3]
-        #print('TempMax was ' + str(entryList[3]))
+        # print('TempMax was ' + str(entryList[3]))
     elif element == 'PRCP':
         entry[element] = entryList[3]
-        #print('Precipitation was ' + str(entryList[3]))
+        # print('Precipitation was ' + str(entryList[3]))
     elif element == 'SNOW':
         entry[element] = entryList[3]
-        #print('Snow fall was ' + str(entryList[3]))
-    #TODO - Add more 'elif' statements for the rest of the ELEMENT/VALUE pairs.
+        # print('Snow fall was ' + str(entryList[3]))
+    # TODO - Add more 'elif' statements for the rest of the ELEMENT/VALUE pairs.
     else:
         print('Found element ' + str(entryList[2]))
 
@@ -131,9 +131,19 @@ print('Resulting Single Day Entry')
 df_entry = pandas.DataFrame([entry])
 print(df_entry)
 
-# 4) Add a new TOTAL DAY entry of weather events in a data frame.
-df_dataSummary = pandas.DataFrame(columns=newHeader)
+# 5) Add a new TOTAL DAY entry of weather events in a data frame.
+df_dataSummary = pandas.DataFrame(columns=DETAILED_HEADER)
 df_dataSummary = pandas.concat([df_dataSummary, df_entry], ignore_index=True)
 print('Resulting Dataframe with ONLY single day summaries.')
 print(df_dataSummary)
 
+############################################
+# Publish a new DataQualityReport on the list of single day data entries.
+report2 = DataQualityReport()
+
+for thisLabel in DETAILED_HEADER:  # for each column, report basic stats
+    thisCol = df_dataSummary[thisLabel]
+    report2.addCol(thisLabel, thisCol)
+
+# print("DataQualityReport - 2/2")
+# print(report2.to_string())
