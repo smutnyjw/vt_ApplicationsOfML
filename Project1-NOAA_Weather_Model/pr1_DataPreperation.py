e16f4e65c7725ce24c3f542ae62367df5cd34e26
SadWheatFarmer <smutnyjw@gmail.com>
2022-03-10 20:53:30 -0500

    PROJECT1: PublishedSummaryData
    Successfully processed ALL of the required data and published a csv of daily summaries of info. Does not include NEXTDAY values.

--- Project1-NOAA_Weather_Model/pr1_DataPreperation.py
+++ Project1-NOAA_Weather_Model/pr1_DataPreperation.py
@@ -20,12 +20,15 @@
 #       Identify every unique Day and process some ELEMENT possibilities into
 #           a single list. Then place that list into a resulting dataframe.
 #       Publish a DataQualityReport for a day's summary of data.
+#       Publish an output csv of day summary's (except NEXTDAY values)
+#
 #
 #   Cannot:
 #       'DataQualityReport.py' lib file cannot handle string entries.
-#       Publish a DataQualityReport for initial data.
 #       End Goal headers do not contain units.
 #       Process all possible ELEMENT values for an unique Day's events
+#       Account for 'NEXTDAY' values.
+#       Publish a DataQualityReport for initial data.
 ##########################################################################
 
 import pandas
@@ -37,7 +40,7 @@ from vt_ApplicationsOfML.Libraries.DataExploration.DataQualityReport import \
 # Initial loading of header-less data and adding a header
 #   Date = yyyymmdd
 #   OBS-Time = hhmm
-FILE_IN = 'C:/Data/USW00013880-Test.csv'
+FILE_IN = 'C:/Data/USW00013880.csv'
 INIT_HEADER = ['ID', 'Date', 'Element', 'Value', 'MeasurementFlag',
                'QualityFlag', 'SourceFlag', 'OBS-Time']
 df = pandas.read_csv(FILE_IN, header=None)
@@ -60,14 +63,6 @@ TARGET_VARIABLES = ['PRECIPFLAG', 'PRECIPAMT', 'NEXTDAYPRECIPFLAG',
 
 CORE5_HEADER = BASE_COLUMNS + CORE5_FEATURES + TARGET_VARIABLES
 DETAILED_HEADER = BASE_COLUMNS + ELEMENT_TYPES.tolist() + TARGET_VARIABLES
-
-
-##########################################################################
-# TODO - Add logic to remove/change data points from the starting dataset.
-# Based on desired quality, remove/change certain data points.
-# Actions List:
-# 1) ...
-# 2) ...
 
 ##########################################################################
 # TODO - Do work necessary to print a Data Quality Report before cleaning.
@@ -92,6 +87,22 @@ for thisLabel in INIT_HEADER:  # for each column, report basic stats
 #print("DataQualityReport - 1/2")
 #print(report1.to_string())
 
+##########################################################################
+# TODO - Decide whether to place this before or after the 1st DataQualityReport.
+# Based on desired quality, remove/change certain data points.
+# Actions List:
+
+# 1) Remove all data points that have a non-NULL QualityFlag
+print(len(df.QualityFlag.isnull()))
+df = df[df.QualityFlag.isnull()]
+
+# 1b) TODO - Get indexes of all MeasurementFlag 'T' values?
+
+# 2) Drop the QualityFlag & SourceFlag column
+df = df.drop(columns=['QualityFlag', 'SourceFlag'])
+print(df[1:5])
+
+
 
 ##########################################################################
 # Create a new dataframe that will have one column per ELEMENT designation.
@@ -109,8 +120,8 @@ else:
 
 # 2) Isolate a single day's total events into a dictionary.
 df_listOfDays = df.Date.unique()
-print("list of unique Days: ")
-print(df_listOfDays)
+#print("list of unique Days: ")
+#print(df_listOfDays)
 
 # TODO - When ready, create another loop to loop over all unique days below.
 if ALL_DAYS:
@@ -121,39 +132,35 @@ else:
 df_dataSummary = pandas.DataFrame(columns=NEW_HEADER)
 
 for dayToProcess in range(0, numDays):
-    oneDayEvents = df.loc[df['Date'] == df_listOfDays[dayToProcess]]
-    # - HERE is issue ^^^. I need to cycle through her until reached null.
-
-    print("Day(s)")
-    print(oneDayEvents.Date)
-    print("IsoDaysEvents:")
-    print(oneDayEvents)
-
     # 3) Loop through each datapoint on a particular day. Find what part of the
     #       dictionary the datapoint's ELEMENT corresponds too. Do math based on
     #       that ELEMENT value.
+    oneDayEvents = df.loc[df['Date'] == df_listOfDays[dayToProcess]]
     eventsInDay = len(oneDayEvents.index)
-    print("Events in the Day Measured:")
-    print(eventsInDay)
+    #print("Events in the Day Measured:")
+    #print(eventsInDay)
 
+    # 4) Summarize the day's events into a single entry
     for i in range(0, eventsInDay):
-        # 4) Summarize the day's events into a single entry
-        #       Create a blank entry.
+        # Dissect each event in a day.
+        index = oneDayEvents.index[i]
+        entryList = oneDayEvents.loc[index, :].values.tolist()
+        #print("Event:")
+        #print(entryList)
+
         if i == 0:
+            # Create a blank entry to summarize a new day.
             blankValues = [0] * len(NEW_HEADER)
             entry = dict(zip(NEW_HEADER, blankValues))
-            print("Blank Entry:")
-            print(entry)
+            #print("Blank Entry:")
+            #print(entry)
+
+            percipFlag = False
+            percipAmt = 0
 
             # Fill in One-Time-Only values.
             entry['Date'] = entryList[1]
             entry['#Events'] = eventsInDay
-
-        # Dissect each event in a day.
-        index = oneDayEvents.index[i]
-        entryList = oneDayEvents.loc[index, :].values.tolist()
-        print("Event:")
-        print(entryList)
 
         # Fill in values for each desired column and ELEMENT
         element = entryList[2]
@@ -162,11 +169,19 @@ for dayToProcess in range(0, numDays):
         if element == 'TMAX':
             entry[element] = entryList[3]
             # print('TempMax was ' + str(entryList[3]))
+            #TODO - use case: What happens if there are more than one of a
+            # measurement?
         elif element == 'PRCP':
             entry[element] = entryList[3]
+            if entryList[3] > 0 or df.MeasurementFlag[index] == 'T':
+                percipFlag = True
+            percipAmt = percipAmt + entryList[3]
             # print('Precipitation was ' + str(entryList[3]))
         elif element == 'SNOW':
             entry[element] = entryList[3]
+            if entryList[3] > 0 or df.MeasurementFlag[index] == 'T':
+                percipFlag = True
+            percipAmt = percipAmt + entryList[3]/8
             # print('Snow fall was ' + str(entryList[3]))
         elif element == 'TMIN':
             entry[element] = entryList[3]
@@ -176,7 +191,12 @@ for dayToProcess in range(0, numDays):
             # print('Snow depth was ' + str(entryList[3]))
         # TODO - Add more 'elif' statements for the rest of the ELEMENT/VALUE pairs.
         else:
-            print('Found element ' + str(entryList[2]))
+            continue
+            #print('Found element ' + str(entryList[2]))
+
+    # After going through all of the days events; fill in precipitation info.
+    entry['PRECIPFLAG'] = percipFlag
+    entry['PRECIPAMT'] = percipAmt
 
     #print('Resulting Single Day Entry')
     df_entry = pandas.DataFrame([entry])
@@ -187,6 +207,9 @@ for dayToProcess in range(0, numDays):
 
 #print('Resulting Dataframe with ONLY single day summaries.')
 #print(df_dataSummary)
+
+##########################################################################
+# TODO - Update headers w/ end units
 
 ############################################
 # Publish a new DataQualityReport on the list of single day data entries.
