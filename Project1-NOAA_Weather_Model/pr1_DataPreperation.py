8363da442e5eac5a30c7c530834a9a8f52a07457
SadWheatFarmer <smutnyjw@gmail.com>
2022-03-11 16:30:22 -0500

    PROJECT1: Ugly PREV & NEXT day calculations
    Successfully can output 0.1% of days including Previous and Next day target values.

--- Project1-NOAA_Weather_Model/pr1_DataPreperation.py
+++ Project1-NOAA_Weather_Model/pr1_DataPreperation.py
@@ -52,14 +52,15 @@ FILE_OUT = 'C:/Data/USW00013880-Test-SINGLEDAY.csv'
 #####################################
 # Establish user settings for the program, dataframe headers, etc
 MODEL_VERSION = 1
-ALL_DAYS = 0
+PERCENT_DAYS = 0.1
 OUTPUT_FILE = 1
 
 BASE_COLUMNS = ['Date', '#Events']
 CORE5_FEATURES = ['PRCP', 'SNOW', 'SNWD', 'TMAX', 'TMIN']
 ELEMENT_TYPES = df.Element.unique()
-TARGET_VARIABLES = ['PRECIPFLAG', 'PRECIPAMT', 'NEXTDAYPRECIPFLAG',
-                    'NEXTDAYPRECIPAMT']
+TARGET_VARIABLES = ['PREV_PRECIPFLAG', 'PREV_PRECIPAMT','PRECIPFLAG',
+                    'PRECIPAMT', 'NEXTPRECIPFLAG',
+                    'NEXTPRECIPAMT']
 
 CORE5_HEADER = BASE_COLUMNS + CORE5_FEATURES + TARGET_VARIABLES
 DETAILED_HEADER = BASE_COLUMNS + ELEMENT_TYPES.tolist() + TARGET_VARIABLES
@@ -88,8 +89,8 @@ for thisLabel in INIT_HEADER:  # for each column, report basic stats
 #print(report1.to_string())
 
 ##########################################################################
-# TODO - Decide whether to place this before or after the 1st DataQualityReport.
-# Based on desired quality, remove/change certain data points.
+# After initial Data Quality Report.
+# Based on desired quality, remove/change certain data points for modeling.
 # Actions List:
 
 # 1) Remove all data points that have a non-NULL QualityFlag
@@ -123,11 +124,11 @@ df_listOfDays = df.Date.unique()
 #print("list of unique Days: ")
 #print(df_listOfDays)
 
-# TODO - When ready, create another loop to loop over all unique days below.
-if ALL_DAYS:
-    numDays = len(df_listOfDays)
+
+if PERCENT_DAYS:
+    numDays = int(len(df_listOfDays) * PERCENT_DAYS)
 else:
-    numDays = 10
+    numDays = 20
 
 df_dataSummary = pandas.DataFrame(columns=NEW_HEADER)
 
@@ -135,16 +136,16 @@ for dayToProcess in range(0, numDays):
     # 3) Loop through each datapoint on a particular day. Find what part of the
     #       dictionary the datapoint's ELEMENT corresponds too. Do math based on
     #       that ELEMENT value.
-    oneDayEvents = df.loc[df['Date'] == df_listOfDays[dayToProcess]]
-    eventsInDay = len(oneDayEvents.index)
+    df_oneDayEvents = df.loc[df['Date'] == df_listOfDays[dayToProcess]]
+    numEventsInDay = len(df_oneDayEvents.index)
     #print("Events in the Day Measured:")
-    #print(eventsInDay)
+    #print(df_oneDayEvents)
 
     # 4) Summarize the day's events into a single entry
-    for i in range(0, eventsInDay):
+    for i in range(0, numEventsInDay):
         # Dissect each event in a day.
-        index = oneDayEvents.index[i]
-        entryList = oneDayEvents.loc[index, :].values.tolist()
+        index = df_oneDayEvents.index[i]
+        entryList = df_oneDayEvents.loc[index, :].values.tolist()
         #print("Event:")
         #print(entryList)
 
@@ -160,7 +161,7 @@ for dayToProcess in range(0, numDays):
 
             # Fill in One-Time-Only values.
             entry['Date'] = entryList[1]
-            entry['#Events'] = eventsInDay
+            entry['#Events'] = numEventsInDay
 
         # Fill in values for each desired column and ELEMENT
         element = entryList[2]
@@ -207,6 +208,53 @@ for dayToProcess in range(0, numDays):
 
 #print('Resulting Dataframe with ONLY single day summaries.')
 #print(df_dataSummary)
+
+##########################################################################
+# Populate precipitation data based on PREVIOUS and NEXT Day.
+for dayToProcess in range(0, numDays):
+    if dayToProcess > 0:
+        df_PrevDay = df_dataSummary.index[df_dataSummary['Date'] ==
+                                         df_listOfDays[dayToProcess-1]]
+    df_curDay = df_dataSummary.index[df_dataSummary['Date'] ==
+                                     df_listOfDays[dayToProcess]]
+    df_NextDay = df_dataSummary.index[df_dataSummary['Date'] ==
+                                      df_listOfDays[dayToProcess+1]]
+
+    # Fill in PREVIOUS day precipitation amounts.
+    if 0 < dayToProcess < numDays-1:
+        #print("PDay: " + str(df_dataSummary.at[df_PrevDay[0], 'Date']))
+        #print("CDay: " + str(df_dataSummary.at[df_curDay[0], 'Date']))
+        #print("NDay: " + str(df_dataSummary.at[df_NextDay[0], 'Date']))
+
+        # Fill in PREVIOUS day precipitation.
+        df_dataSummary.at[df_curDay[0], 'PREV_PRECIPFLAG'] = \
+                                df_dataSummary.at[df_PrevDay[0], 'PRECIPFLAG']
+        df_dataSummary.at[df_curDay[0], 'PREV_PRECIPAMT'] = \
+                                df_dataSummary.at[df_PrevDay[0], 'PRECIPAMT']
+
+        # Fill in NEXT day precipitation.
+        df_dataSummary.at[df_curDay[0], 'NEXTPRECIPFLAG'] = \
+            df_dataSummary.at[df_NextDay[0], 'PRECIPFLAG']
+        df_dataSummary.at[df_curDay[0], 'NEXTPRECIPAMT'] = \
+            df_dataSummary.at[df_NextDay[0], 'PRECIPAMT']
+
+    elif dayToProcess == 0:
+        df_dataSummary.at[df_curDay[0], 'PREV_PRECIPFLAG'] = 0
+        df_dataSummary.at[df_curDay[0], 'PREV_PRECIPAMT'] = 0
+        df_dataSummary.at[df_curDay[0], 'NEXTPRECIPFLAG'] = \
+            df_dataSummary.at[df_NextDay[0], 'PRECIPFLAG']
+        df_dataSummary.at[df_curDay[0], 'NEXTPRECIPAMT'] = \
+            df_dataSummary.at[df_NextDay[0], 'PRECIPAMT']
+    else:
+        df_dataSummary.at[df_curDay[0], 'PREV_PRECIPFLAG'] = \
+                                    df_dataSummary.at[df_PrevDay[0], 'PRECIPFLAG']
+        df_dataSummary.at[df_curDay[0], 'PREV_PRECIPAMT'] = \
+                                    df_dataSummary.at[df_PrevDay[0], 'PRECIPAMT']
+        df_dataSummary.at[df_curDay[0], 'NEXTPRECIPFLAG'] = 0
+        df_dataSummary.at[df_curDay[0], 'NEXTPRECIPFLAG'] = 0
+
+    #print(df_dataSummary.loc[df_dataSummary['Date'] == df_listOfDays[
+    # dayToProcess]])
 
 ##########################################################################
 # TODO - Update headers w/ end units
