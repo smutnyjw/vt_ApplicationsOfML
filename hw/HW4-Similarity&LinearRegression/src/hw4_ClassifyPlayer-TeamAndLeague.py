ad138e0df35e8611e093af168a62fea08bf40fd9
SadWheatFarmer <smutnyjw@gmail.com>
2022-04-01 22:35:16 -0400

    HW4: Part1 Complete
    Last additions to Part1 Classification model outputs. Added count of values effected by each data prep change, Best/Worst k value and accuracy, and resulting plots for both RandomSeeds.

--- hw/HW4-Similarity&LinearRegression/src/hw4_ClassifyPlayer-TeamAndLeague.py
+++ hw/HW4-Similarity&LinearRegression/src/hw4_ClassifyPlayer-TeamAndLeague.py
@@ -75,30 +75,55 @@ FEATURES_TO_NORMALIZE = [
 ################################################################################
 
 def cleanRawData(df: pd.DataFrame) -> pd.DataFrame:
-    ''' Clean undesired data from the raw NOAA data frame '''
-    print("Size of Initial Dataset: {}".format(len(df)))
+    ''' Clean undesired data from the inputted data '''
+    print("Begin Data Preparation...")
+
+    # Keep track of number of values effected by each step
+    orig_size = len(df)
+    numDropped = 0
+    numNULL = 0
+    numAbove1 = 0
+    numBelow0 = 0
+    numHigh = 0
 
     # 1) Remove all entries that are not from the 2016 season
     df = df.query("yearID >= 2016")
-    print("Size of Dataset with only 2016 entries: {}".format(len(df)))
+    numDropped = orig_size - len(df)
 
     # 2) Drop the quality_flag & source_flag column
     df = df.drop(columns=['playerID', 'yearPlayer'])
+    numDropped = numDropped - len(df)
 
     # 3) Replace all NULL or #N/A values with the median of that feature
     for label in df.columns:
         col = df[label]
 
         if col.isnull().sum() != 0:
+            numNULL = numNULL + col.isnull().sum()
             medianV = col.median()
             df[label] = col.fillna(medianV)
 
         # 4) Set a [0, 1] clamp on every 'Rate' feature (*rat in the name).
         if 'rat' in label:
             if col.max() > 1:
+                numAbove1 = numAbove1 + np.sum(col > 1)
                 col.where(col <= 1, 1, inplace=True)
             if col.min() < 0:
+                numBelow0 = numBelow0 + np.sum(col < 0)
                 col.where(col >= 0, 0, inplace=True)
+
+        # 5) Check for invalidly high values (>1000) replace with 0
+        if type(col[col.first_valid_index()]) != str and \
+                label != 'Salary' and label != 'yearID':
+            numHigh = numHigh + np.sum(col > 1000)
+            col.where(col < 1000, 0, inplace=True)
+
+    print("Data Prep # Changed Values out of {}:\n#Dropped: {}\n#NULL: {}"
+          "\n#AboveOne: {}\n#BelowZero: {}\n#HighValues: {}".format(
+                                                orig_size, numDropped, numNULL,
+                                                numAbove1, numBelow0, numHigh))
+
+    print("Data Preparation COMPLETE...")
 
     return df
 
@@ -118,7 +143,7 @@ report2.quickDQR(df_stats, list(df_stats.columns))
 
 # Normalize all numeric features.
 for label in FEATURES_TO_NORMALIZE:
-    df_stats[label] = df_stats[label]/df_stats[label].max()
+    df_stats[label] = df_stats[label] / df_stats[label].max()
 report3 = DataQualityReport()
 report3.quickDQR(df_stats, list(df_stats.columns))
 
@@ -126,7 +151,6 @@ if OUTPUT_FILES:
     report1.to_excel(OUTPUT_DQR1)
     report2.to_excel(OUTPUT_DQR2)
     report3.to_excel(OUTPUT_DQR3)
-
 
 ################################
 # Create kNN Model
@@ -143,7 +167,6 @@ x_test = test.drop(columns=['teamID', 'lgID'])
 y_test_lgID = pd.get_dummies(test.lgID, prefix='lg')
 y_test_teamID = pd.get_dummies(test.teamID, prefix='teamID')
 
-
 ################################
 # Train the 'lgID' and 'TeamId' classifiers and calculate accuracy
 rangeK = range(MIN_K, MAX_K_exclusive)
@@ -152,7 +175,8 @@ teamId_accuracy = np.zeros(len(rangeK))
 
 for i in range(MAX_K_exclusive - MIN_K):
     k = i + MIN_K
-    print("Training Model k = {} out of {} Starting...".format(k, MAX_K_exclusive - 1))
+    print("Training Model k = {} out of {} Starting...".format(k,
+                                                               MAX_K_exclusive - 1))
 
     clf = neighbors.KNeighborsClassifier(n_neighbors=k, weights='uniform')
     clf.fit(x_train, y_train_lgId)
@@ -164,14 +188,28 @@ for i in range(MAX_K_exclusive - MIN_K):
     result_teamId = clf.predict(np.asarray(x_test))
     teamId_accuracy[i] = clf.score(x_test, y_test_teamID)
 
-
 ################################
+# Display Results
+best_k_lgId = np.where(lgId_accuracy == max(lgId_accuracy))
+best_k_teamId = np.where(teamId_accuracy == max(teamId_accuracy))
+print("RESULTS: Best (k/accuracy):\n\tlgId = {}/{}\n\tteamId = {},"
+      "{}".format(best_k_lgId[0]+1, max(lgId_accuracy),
+                  best_k_teamId[0]+1, max(teamId_accuracy)))
+
+worst_k_lgId = np.where(lgId_accuracy == min(lgId_accuracy))
+worst_k_teamId = np.where(teamId_accuracy == min(teamId_accuracy))
+print("RESULTS: Worst (k/accuracy):\n\tlgId = {}/{}\n\tteamId = {},"
+      "{}".format(worst_k_lgId[0]+1, min(lgId_accuracy),
+                  worst_k_teamId[0]+1, min(teamId_accuracy)))
+
 # Plot Classification Accuracy chart of both features 'lgID' and 'teamID'.
 fig = plt.figure()
 id_scatter = fig.add_subplot(111)
 
 id_scatter.scatter(rangeK, lgId_accuracy, marker='o', label='lg')
 id_scatter.scatter(rangeK, teamId_accuracy, marker='^', label='team')
+plt.title("kNN Accuracy of MLB Player Teams & League Affiliation - seed {"
+          "}".format(RANDOM_SEED))
 plt.xlabel("k (# of Nearest Neighbors)")
 plt.ylabel("Accuracy of BOTH Classifications")
 plt.legend(loc='best')
