108919ace75b2cab989f11bb51ed8111f6ac4a55
SadWheatFarmer <smutnyjw@gmail.com>
2022-04-13 20:45:51 -0400

    HW5: Finished iteration
    Created iteration loop. Used modulus to cycle through x possible combinations.

--- hw/HW5-FindBestNeuralNetwork/src/hw5_FindBestNeuralNetwork.py
+++ hw/HW5-FindBestNeuralNetwork/src/hw5_FindBestNeuralNetwork.py
@@ -32,15 +32,16 @@ from vt_ApplicationsOfML.Libraries.DataExploration.DataQualityReport import \
 
 DEBUG = False
 OUTPUT_FILES = False
-TRAIN_DATA = 0.7
+TRAIN_DATA = 0.8
 TEST_DATA = 0.2
+VALID_DATA_FROM_TRAIN = 0.25
 RANDOM_SEED = 23
 DATA_USE_RATIO = 0.1
 
 # Neural Network Qualities to be tested.
-RANGE_HL = range(1,3)
-RANGE_NODES = range(1,10)
-RANGE_ActFcts = ['relu', 'logistic', 'identity', 'tanh']
+RANGE_HL = range(1, 4)          #range(1, 3)
+RANGE_NODES = range(1, 4)      #range(1, 10)
+RANGE_ActFcts = ['relu']      #['relu', 'logistic', 'identity', 'tanh']
 
 
 ## Control flags and constants
@@ -96,11 +97,12 @@ def chooseBestANN(x: pd.DataFrame, y: pd.DataFrame, hls, nodes, actFcts) -> [
     #   3) # of Nodes per Hidden Layer
 
     # Set up train, validation, and test data
-    trainX, trainY, validX, validY, testX, testY = \
-        modelsel.train_test_split(x, y,
-                                  valid_size=TRAIN_DATA,
-                                  test_size=TEST_DATA,
-                                  random_state=RANDOM_SEED)
+    trainX, testX, trainY, testY = modelsel.train_test_split(x, y,
+                                                test_size=TEST_DATA,
+                                                random_state=RANDOM_SEED)
+    trainX, validX, trainY, validY = modelsel.train_test_split(trainX, trainY,
+                                                test_size=VALID_DATA_FROM_TRAIN,
+                                                random_state=RANDOM_SEED)
 
     # Define an array of empty hidden layers based on the max number of
     # layers tested.
@@ -111,11 +113,27 @@ def chooseBestANN(x: pd.DataFrame, y: pd.DataFrame, hls, nodes, actFcts) -> [
     for fct in actFcts:
 
         # Define how many hidden layers are in this iteration
-        for nhls in hls:
+        for numHLs in hls:
 
-            # Set the hidden layers for the iteration
-            for i in range(nhls):
-                #hl[i] = 1
+            # Define the max number of nodes per HL are in this iteration
+            #for maxNumNodes in range(len(RANGE_NODES)):
+            maxNumNodes = len(RANGE_NODES)
+
+
+            # Generate a counter to set the hnumber of nodes in each HL
+            for i in range(((maxNumNodes+1)**numHLs)):
+                i = i
+                hl[0] = i % (maxNumNodes+1)
+                hl[1] = int(i/(maxNumNodes+1)) % (maxNumNodes+1)
+                hl[2] = int(i/(((maxNumNodes+1)**2)))
+                print("Instance {}/{}/{}: {}, {}, {}".format(numHLs,
+                                                             maxNumNodes,
+                                                             i,
+                                                             hl[0],
+                                                             hl[1],
+                                                             hl[2]))
+            print("-----------------")
+
 
 
 
@@ -134,7 +152,10 @@ if OUTPUT_FILES:
 [x, y] = prepareData(df_raw)
 
 # Determine the best artificial Neural Network
-[bestANN, df_ErrorData] = chooseBestANN(x, y)
+[bestANN, df_ErrorData] = chooseBestANN(x, y,
+                                        RANGE_HL,
+                                        RANGE_NODES,
+                                        RANGE_ActFcts)
 
 
 # Plot the best ANN error data.
